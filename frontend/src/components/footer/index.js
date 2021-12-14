3847024920a65adb32c3e94866fbb30bac9c42e7
dammaer <advtmb@ya.ru>
2021-12-14 16:09:36 +0300

    второе ревью

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -1,10 +1,17 @@
 import styles from './style.module.css'
 import { Container, LinkComponent } from '../index'
+import cn from 'classnames'
+
+const footerLink = cn(styles.footer__link, styles.footer__brand)
 
 const Footer = () => {
   return <footer className={styles.footer}>
       <Container className={styles.footer__container}>
         <LinkComponent href='#' title='Продуктовый помощник' className={styles.footer__brand} />
+        <div className={styles.footer__links}>
+          <a className={footerLink} href="https://github.com/dammaer">Автор</a>
+          <a className={footerLink} href='https://github.com/dammaer/foodgram-project-react#readme'>Технологии</a>
+        </div>
       </Container>
   </footer>
 }
